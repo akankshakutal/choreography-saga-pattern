98b99ae1863fb4f8cf1545e6a5e84afe676e702e
bilal-fazlani <bilal.m.fazlani@gmail.com>
2021-09-29 00:00:42 +0530

    go back to older zio version due to bug\nhttps://github.com/zio/zio/issues/5530

--- product-service/project/Libs.scala
+++ product-service/project/Libs.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Libs {
-  lazy val zioStream = "dev.zio" %% "zio-streams" % "1.0.12"
-  lazy val zio = "dev.zio" %% "zio" % "1.0.12"
+  lazy val zioStream = "dev.zio" %% "zio-streams" % "1.0.10"
+  lazy val zio = "dev.zio" %% "zio" % "1.0.10"
   lazy val zioKafka = "dev.zio" %% "zio-kafka" % "0.16.0"
   lazy val zioJson = "dev.zio" %% "zio-json" % "0.2.0-M1"
   lazy val zioConfig = "dev.zio" %% "zio-config-magnolia" % "1.0.10"

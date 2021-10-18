8955da06ddd7affbe174790e9de08a058392f814
Ashish Bhalgat <ashish.bhalgat@thoughtworks.com>
2021-10-18 20:06:31 +0530

    Ashish|Mohit Added initial Shipment service code

---
+++ shipment-service/Shipment/kafka/KafkaConsumer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+
+namespace Shipment.kafka
+{
+    public class KafkaConsumer
+    {
+        private ConsumerConfig _config = null;
+        private KafkaProducer _producer = null;
+        private string _publisherTopic = string.Empty;
+         public KafkaConsumer(IConfiguration Configuration , KafkaProducer producer)
+        {
+            var endpoint = (string)Configuration.GetSection("Kafka").GetValue(typeof(string), "endpoint");
+            var consumerGroupName = (string)Configuration.GetSection("Kafka").GetValue(typeof(string), "consumerGroup");
+            _publisherTopic = (string)Configuration.GetSection("Kafka").GetValue(typeof(string), "publisherTopic");
+
+
+            _config = new ConsumerConfig
+            {
+                BootstrapServers = endpoint,
+                GroupId = consumerGroupName,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            _producer = producer;
+
+        }
+
+        public void Consume(string topicName)
+        {
+            using (var consumer = new ConsumerBuilder<Ignore, string>(_config).Build())
+            {
+                consumer.Subscribe(topicName);
+
+                while (true)
+                {
+                    var consumeResult = consumer.Consume();
+                    var message = consumeResult.Message.Value;
+                    Console.WriteLine($"Message Recieved : {message}");
+
+                    // handle consumed message.
+                    _producer.PublishMessage(message, _publisherTopic);
+
+                }
+
+                consumer.Close();
+            }
+        }
+
+
+    }
+}
+
---
+++ shipment-service/Shipment/kafka/KafkaProducer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+
+namespace Shipment.kafka
+{
+    public class KafkaProducer
+    {
+        private ProducerConfig _config = null;
+        public KafkaProducer(IConfiguration Configuration)
+        {
+
+           var endpoint = (string)Configuration.GetSection("Kafka").GetValue(typeof(string),"endpoint");
+           _config = new ProducerConfig
+            {
+              
+                BootstrapServers = endpoint,
+                ClientId = Dns.GetHostName()
+
+            };
+
+        }
+
+        public void PublishMessage(string message, string topicName)
+        {
+            using (var producer = new ProducerBuilder<Null, string>(_config).Build())
+            {
+                producer.Produce(topicName, new Message<Null, string> { Value = message});
+            }
+        }
+    }
+}
+
